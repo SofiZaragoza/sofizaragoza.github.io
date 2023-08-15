90c160a669f610d86c43e096ebc53725c9156424
SofiZaragoza <129299243+SofiZaragoza@users.noreply.github.com>
2023-08-15 14:27:38 +0200

    Add files via upload

--- budget.js
+++ budget.js
@@ -1,11 +1,12 @@
 
-const Expenselist = []; //Array para almacenar datos
+const Expenselist = []; //Array para almacenar datos de las expenses
 
 const form = document.getElementById("incomeform");
 const formExpense = document.getElementById("expenseForm");
 const tablabody = document.getElementById("tablaexpense");
 const finalbudget = document.getElementById("totalbudget");
 
+//Expense data para crear las expenses nuevas
 class expense {
     constructor (naming,Expname,amount,type){
         this.Name = naming;
@@ -15,21 +16,27 @@ class expense {
     }
 }
 
-let campoIncome = 0; // Declare campoIncome at a higher scope
+let campoIncome = 0; // Declare campoIncome fuera de la función para utilizarlo después
 
 function submitIncome() {
-    // Get the input value + pareafloat 
+    // Get the input value + pareafloat
     campoIncome = parseFloat(document.getElementById("incomeinput").value);
 
-    // valido los datos que sean nñumeros y mayores a cero con un if
+    // valido los datos que sean números y mayores a cero con un if
     if(!isNaN(campoIncome) && campoIncome > 0){
         document.getElementById("incomeinput").textContent = campoIncome;// Display the income value on the page
-        localStorage.setItem('income', JSON.stringify(campoIncome));// Store income in local storage
+        localStorage.setItem('income', JSON.stringify(campoIncome));// Store income in local storage 
     }else{
-        alert("Por favor, ingresa un número mayor a 0");
+        Swal.fire({/*reemplazo el alert por swal.fire para notificar los problemas*/
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please, do insert number greater than 0 ',
+          })
+        
+        document.getElementById("incomeinput").textContent = '';
     }
 }
-    //agrego un addevent listener para validar el boton submit que viene del campo del form
+    //agrego un addevent listener para validar el boton submit del campoIncome
     form.addEventListener("submit",(ev) =>{
         ev.preventDefault();
         submitIncome()
@@ -39,19 +46,20 @@ function submitIncome() {
         firstbudget.textContent = `Current Budget:$ ${campoIncome}`;
     })
 
-
-    
-
-let sumadegastos = 0; 
+let sumadegastos = 0; //hacemos una función submitexpense para crear los datos, validar los datos y renderizarlo en una tabla.
 
 function submitExpense() {
     let username = document.getElementById("userName").value;
     let expensename =  document.getElementById("expenseName").value;
     let expensetype = document.getElementById("expenseType").value;
     let expenseamount =  parseFloat(document.getElementById("expenseAmount").value);
-    //valido que tena un mín de palabras, los numeros con una expresión regular  
+    //valido que tenga un mín de palabras, los numeros con una expresión regular (tuve que pedir ayuda en coderhouse) 
     if(!/^[A-Za-z]{3,}$/.test(username) || isNaN(expenseamount)||expenseamount <= 0 ){//usamos una expresión regular para validar los datos
-        alert("Hay campos erroneos en el formulario");
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please, names are not alphanumeric (only letters) & expenses should be greater than 0 ',
+          })
     }else{
         document.getElementById("userName").textContent = username;
         const newExpense = new expense(username,expensename,expenseamount,expensetype);
